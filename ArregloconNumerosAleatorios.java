d030bb20929b1a2f33927b280f954f372fbb5d28
Reyesoto335 <elmersoto123@gmail.com>
2022-02-05 13:24:52 -0600

    Arreglo del código e implementación de arreglo tipo borbuja y quiksort

--- ArregloconNumerosAleatorios.java
+++ ArregloconNumerosAleatorios.java
@@ -8,11 +8,16 @@ public class ArregloconNumerosAleatorios {
     static int [] NumerosAleatorios = new int [10];
     static int [] NumerosAleatorios2 = new int [10];
     static int [] NumerosAleatorios3 = new int [10];
-    static int min = 1;
-    static int max = 100;
+    static int min = 1, max = 100;
+    static int aux, mayor, menor;
     public static void main(String[] args) {
         llenado_de_arreglos();
-        first_array();
+        for (int i = 0; i < NumerosAleatorios.length; i++) {
+            System.out.println(i + "==>" + NumerosAleatorios[i]);
+        }
+        //Metodo_borbuja();
+        Metodo_quiksort(0,NumerosAleatorios.length-1);
+        //first_array();
         second_array();
         third_array();
         muestreo();
@@ -50,6 +55,62 @@ public class ArregloconNumerosAleatorios {
             System.out.println(i+" ");
         }*/
         
+    }
+    
+    public static void Metodo_quiksort(int bajo, int alto){
+        if (bajo >=  alto) {
+            return;
+        }
+        int inicio = bajo;
+        int end = alto;
+        int base = NumerosAleatorios[bajo];
+        
+        while(inicio < end){
+            while(inicio < end && NumerosAleatorios[end] >= base){
+                end--;
+            }
+        
+            if(NumerosAleatorios[end] <= base){
+                int temporal = NumerosAleatorios[end];
+                NumerosAleatorios[end] = NumerosAleatorios[inicio];
+                NumerosAleatorios[inicio] = temporal;
+            }
+        
+            while(inicio < end && NumerosAleatorios[inicio] <= base){
+                inicio++;
+            }
+        
+            if (NumerosAleatorios[inicio] >= base){
+                int temporal = NumerosAleatorios[end];
+                NumerosAleatorios[end] = NumerosAleatorios[inicio];
+                NumerosAleatorios[inicio] = temporal;
+            }
+        }
+        
+        Metodo_quiksort(bajo, inicio-1);
+        Metodo_quiksort(end+1, alto);
+        
+    }
+    
+    public static void Metodo_borbuja(){
+        mayor = menor = NumerosAleatorios [0];
+            for (int i = 0; i < 10; i++){
+                if (NumerosAleatorios [i] > mayor){
+                    mayor = NumerosAleatorios[i];
+                }
+                if (NumerosAleatorios [i] < menor){
+                    menor = NumerosAleatorios[i];
+            } 
+        }
+        for (int i = 0; i < 10 - 1; i++) {
+            for (int j = 0; j < 10 - 1; j++) {
+                if (NumerosAleatorios [j] > NumerosAleatorios[j+1]) {
+                    aux = NumerosAleatorios[j];
+                    NumerosAleatorios[j] = NumerosAleatorios[j+1];
+                    NumerosAleatorios [j+1] = aux;
+                }
+            }
+        }
     }
     
     public static void first_array(){
